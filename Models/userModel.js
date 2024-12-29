c275e4d688fe1954c0148b71b4d64a68b0268b63
Simeoncoded <simeonolawore07@gmail.com>
2024-12-29 01:51:15 -0500

    encrypting password

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -30,6 +31,19 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        return next();      
+    }
+
+    //encrypt password with bcryptjs
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.confirmPassword = undefined;
+
+});
+
 
 const User = mongoose.model('User', userSchema);
 
